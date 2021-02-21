daec972c6bf222ad2609f7991366915dcad20f99
Rolando Martinez Gallardo <rolando.martinez@scaleaq.com>
2021-02-20 22:01:07 -0300

    workaround for initialize tenant datacontext

--- BasePathStrategySample/Controllers/HomeController.cs
+++ BasePathStrategySample/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Farmer.Data;
+using Farmer.Data.Model;
 using Finbuckle.MultiTenant;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,17 +9,45 @@ namespace BasePathStrategySample.Controllers
 {
     public class HomeController : Controller
     {
-        private FarmerDataContext _context;
-        public HomeController(FarmerDataContext context)
-        {
-            _context = context;
-        }
+        //private FarmerDataContext _context;
+        //public HomeController(FarmerDataContext context)
+        //{
+        //    _context = context;
+        //}
         
         public IActionResult Index()
         {
-            var transactions = _context.Transactions.ToList();
+           
             var ti = HttpContext.GetMultiTenantContext<TenantInfo>()?.TenantInfo;
-            return View(ti);
+            if (ti == null)
+                return View(new TransactionViewModel()
+                {
+                    TenantInfo = null,
+                    Transactions = new List<Transaction>()
+                });
+            
+            using (var context = new FarmerDataContext(ti))
+            {
+                var transactions = context.Transactions.ToList();
+                return View(new TransactionViewModel()
+                {
+                    TenantInfo = ti,
+                    Transactions = transactions
+                });
+            }
+
+
+
+
+          
         }
     }
+
+
+    public class TransactionViewModel
+    {
+        public TenantInfo TenantInfo { get; set; }
+        public List<Transaction> Transactions { get; set; }
+    }
+
 }
